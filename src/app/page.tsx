51a7acdfdde041570bf1177d9cf67ef7f5407032
Lesaun Harvey <lesaun@gmail.com>
2023-02-25 20:16:34 -0800

    add tailwind/daisyui; header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import Link from 'next/link'
+import Header from 'src/components/Header'
 import { getUser } from 'src/utils/auth'
 
 export default async function Home() {
   const user = await getUser()
 
   return (
-    <main>
-      <div>
-        {(user.email === 'anonymous'
-          ? <Link href="/api/auth/login" target="_blank">Login</Link>
-          : <Link href="/api/auth/logout">Logout</Link>)}
-        <br />
-        <br />
-        {JSON.stringify(user)}
-      </div>
-    </main>
+    <>
+      <Header user={user} />
+      <main>
+      </main>
+    </>
   )
 }
