887e963519b616dbd37bd063a0e2baa2e0ec5449
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2023-02-26 16:10:40 -0800

    fix: merge error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,8 @@ import { getUser } from "@/utils/auth"
 export default async function Home() {
   const user = await getUser()
 
-  return (user.email !== 'anonymous'
-    // @ts-expect-error Server Component
-    ? <MentatMind />
-    : <>
+  return (
+    <>
       <Header user={user} />
 			{user.email !== 'anonymous' || process.env.NODE_ENV === "development"
 			?
