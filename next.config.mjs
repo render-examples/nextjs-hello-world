7e7c31e370c3c42e9839b25d3ef7f3a706319d31
metonym <ericyl.us@gmail.com>
2024-03-15 12:27:49 -0700

    Upgrade Next to v14

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Uncomment the following line to build a static site.
+  // output: "export",
+
+  reactStrictMode: true,
+};
+
+export default nextConfig;
