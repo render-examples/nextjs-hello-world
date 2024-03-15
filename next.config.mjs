6d1efb01a37ccf11e7063585e39acc6206a5194a
metonym <ericyl.us@gmail.com>
2024-03-15 12:39:46 -0700

    Use static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Uncomment the following line to build a static site.
-  // output: "export",
+  output: "export",
 
   reactStrictMode: true,
 };
